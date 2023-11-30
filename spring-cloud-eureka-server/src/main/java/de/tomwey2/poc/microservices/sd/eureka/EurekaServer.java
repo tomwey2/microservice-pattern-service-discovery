3b4a8f2a10f2f176d308dfd8fee7752bb24c2b7f
Thomas <tomwey2@gmail.com>
2023-11-30 10:34:29 +0400

    added modules config-server and eureka-server

---
+++ spring-cloud-eureka-server/src/main/java/de/tomwey2/poc/microservices/sd/eureka/EurekaServer.java
@@ -0,0 +1,15 @@
+package de.tomwey2.poc.microservices.sd.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+}
