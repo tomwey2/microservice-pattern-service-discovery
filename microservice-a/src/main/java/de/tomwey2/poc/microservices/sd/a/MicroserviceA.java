b004c1ebfa01b94f13633e265e6410f4b6aa224b
Thomas <tomwey2@gmail.com>
2023-12-12 11:20:24 +0400

    added modules MicroserviceA and MicroserviceB

---
+++ microservice-a/src/main/java/de/tomwey2/poc/microservices/sd/a/MicroserviceA.java
@@ -0,0 +1,7 @@
+package de.tomwey2.poc.microservices.sd.a;
+
+public class MicroserviceA {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ microservice-b/src/main/java/de/tomwey2/poc/microservices/sd/b/MicroserviceB.java
@@ -0,0 +1,7 @@
+package de.tomwey2.poc.microservices.sd.b;
+
+public class MicroserviceB {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
